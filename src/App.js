8ba7321a4f3371eb0dbc88bf7bbe824f5fe34ca7
ADOFO-09 <emmanueladofo011@gmail.com>
2023-02-08 23:36:24 +0000

    trying to add conditional rendering

--- src/App.js
+++ src/App.js
@@ -2,14 +2,24 @@ import {useState} from 'react';
 
 function App(){
   const[location, setlocation] = useState({latitude: 25});
+  const[Message, setMessage] = useState({error: null});
   {
     navigator.geolocation.getCurrentPosition(
     (position) => setlocation({latitude:position.coords.latitude})
     );
-    return(
-      <div>{location.latitude}</div>
-    )
+    (error) => setMessage({error: "Invalid location"}) 
   }
+
+  if(location.latitude && !Message.error){
+    return <div>{location.latitude}</div>
+  }
+  if(!location.latitude && Message.error){
+    return <div>{Message.error}</div>
+  }
+  else{
+    <div>Loading....</div>
+  }
+  
 }
 
 export default App;
