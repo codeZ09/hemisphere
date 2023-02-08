0c9d779c1cddc7d7da3de9bfeae7b222a01ed461
Yusif <am.yusif530@gmail.com>
2023-02-07 17:54:31 -0800

    I have made a little change

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import {useState} from 'react';
+import { useState } from "react";
 
-function App(){
-  const[location, setlocation] = useState({latitude: 25});
+function App() {
+  const [location, setlocation] = useState({ latitude: 25, longitude: null });
   {
-    navigator.geolocation.getCurrentPosition(
-    (position) => setlocation({latitude:position.coords.latitude})
+    navigator.geolocation.getCurrentPosition((position) =>
+      setlocation({
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+      })
     );
-    return(
-      <div>{location.latitude}</div>
-    )
+    return (
+      <>
+        <div>{location.latitude}</div>
+        <div>{location.longitude}</div>
+      </>
+    );
   }
 }
 
