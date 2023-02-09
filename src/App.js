7bf807235a64606b0a0a3f00305e21e5b93a63ac
ADOFO-09 <emmanueladofo011@gmail.com>
2023-02-09 00:08:05 +0000

    dealing with conflict

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 import { useState } from "react";
 
-<<<<<<< HEAD
-function App(){
-  const[location, setlocation] = useState({latitude: 25});
-  const[Message, setMessage] = useState({error: null});
-=======
+
 function App() {
   const [location, setlocation] = useState({ latitude: 25, longitude: null });
->>>>>>> 0c9d779c1cddc7d7da3de9bfeae7b222a01ed461
   {
     navigator.geolocation.getCurrentPosition((position) =>
       setlocation({
@@ -21,20 +16,10 @@ function App() {
         <div>{location.longitude}</div>
       </>
     );
-<<<<<<< HEAD
-    (error) => setMessage({error: "Invalid location"}) 
-=======
->>>>>>> 0c9d779c1cddc7d7da3de9bfeae7b222a01ed461
-  }
-
-  if(location.latitude && !Message.error){
-    return <div>{location.latitude}</div>
-  }
-  if(!location.latitude && Message.error){
-    return <div>{Message.error}</div>
-  }
-  else{
-    <div>Loading....</div>
+   return(
+    <div>{location.latitude}</div>
+   )
+    
   }
   
 }
