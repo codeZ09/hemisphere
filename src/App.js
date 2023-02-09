9e88cac2707482aa9b27e0e36c1051f06afc9d47
ADOFO-09 <emmanueladofo011@gmail.com>
2023-02-09 10:31:10 +0000

    trying to do error conditional rendering

--- src/App.js
+++ src/App.js
@@ -3,23 +3,22 @@ import { useState } from "react";
 
 function App() {
   const [location, setlocation] = useState({ latitude: 25, longitude: null });
+  const[info, setinfo] = useState({error: null});
   {
     navigator.geolocation.getCurrentPosition((position) =>
       setlocation({
         latitude: position.coords.latitude,
         longitude: position.coords.longitude,
-      })
+      }),
     );
+    setinfo({error: "User Geolocation Denied"})
     return (
       <>
         <div>{location.latitude}</div>
         <div>{location.longitude}</div>
+        <div>{info}</div>
       </>
-    );
-   return(
-    <div>{location.latitude}</div>
-   )
-    
+    );    
   }
   
 }
